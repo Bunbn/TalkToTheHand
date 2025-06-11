7434b09998702e74a71379ff55ba3820201d1a5e
Bunbn <adashkanadjian@protonmail.com>
2025-06-10 21:26:46 -0700

    For plotting accuracy

--- train.py
+++ train.py
@@ -8,7 +8,12 @@ from tensorflow.keras.utils import to_categorical
 DATA_PATH = 'data/'
 X, y = [], []
 
-label_map = {'A': 0, 'B': 1}  # Expand this
+label_map = {
+    'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7,
+    'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15,
+    'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23,
+    'Y': 24, 'Z': 25
+}  # Expand this
 
 for file in os.listdir(DATA_PATH):
     key = file.split('_')[0]
@@ -30,6 +35,30 @@ model = Sequential([
 ])
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=30, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, epochs=30, validation_data=(X_test, y_test))
 
 model.save('model/asl_model.h5')
+import matplotlib.pyplot as plt
+
+# Plot Accuracy
+plt.figure(figsize=(12, 5))
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val Accuracy')
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+
+# Plot Loss
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Val Loss')
+plt.title('Model Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
